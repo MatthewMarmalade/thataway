f01c6eab61dbd5fd2cb8d2d881a092484f782357
Matthew Marsland <marslandm@me.com>
2020-01-02 04:37:36 +0000

    Colors, List View Headings, Bugfixes

---
+++ Compass/ColorList.swift
@@ -0,0 +1,13 @@
+//
+//  ColorList.swift
+//  Compass
+//
+//  Created by Matthew Marsland on 1/1/20.
+//  Copyright © 2020 Tectane. All rights reserved.
+//
+
+import UIKit
+
+class ColorList: NSObject {
+
+}
---
+++ Compass/ColorSliders.swift
@@ -0,0 +1,21 @@
+//
+//  ColorSliders.swift
+//  Compass
+//
+//  Created by Matthew Marsland on 12/31/19.
+//  Copyright © 2019 Tectane. All rights reserved.
+//
+
+import UIKit
+
+class ColorSliders: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
